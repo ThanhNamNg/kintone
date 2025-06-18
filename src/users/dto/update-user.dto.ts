e25b3c920ec4336c5481acf2483a5cf98fef857b
ThanhNamNg <thanhnam052002@gmail.com>
2025-06-18 17:01:00 +0700

    5:00 18/06/2025

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,9 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+   
+}
+
+
+// This class extends CreateUserDto and makes all its properties optional
